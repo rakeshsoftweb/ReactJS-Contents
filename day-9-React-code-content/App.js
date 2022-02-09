84894796cb305f61b01eead02ce12a47a0b5f281
Rakesh Kumar <rakeshsoftweb@gmail.com>
2022-02-09 12:18:00 +0530

    Add files via upload
    added

---
+++ day-9-React-code-content/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Employee3 from  './Employee3' ;
+function App() {
+  return (
+    <div className="App">
+      <Employee3/>
+    </div>
+  );
+}
+
+export default App;
+
+
